f43781d7202c5270ca827c6727e5aea9e53398dc
Pablo Cornejo <pcornejo@icloud.com>
2021-04-27 08:18:57 -0400

    Update and add new Example previews

--- Sources/Charties/Example/Example.swift
+++ Sources/Charties/Example/Example.swift
@@ -7,11 +7,13 @@
 
 import SwiftUI
 
-struct Example: View {
+struct Example<Marker: View>: View {
+    let lineColors: [Color]
+    let marker: Marker
+    var markerSize: CGSize = .init(width: 16, height: 16)
     
     let data: ScatterData = {
-        var data = ScatterData([(x: 0, y: -0.25),
-                                (x: 1, y: -0.25),
+        var data = ScatterData([(x: 1, y: -0.25),
                                 (x: 2, y: 2),
                                 (x: 3, y: 1),
                                 (x: 4, y: 2.5),
@@ -20,32 +22,51 @@ struct Example: View {
                                 (x: 7, y: 2),
                                 (x: 9, y: 1),
                                 (x: 10, y: 2.75)],
-                               xAxisTitle: nil,
-                               yAxisTitle: "pH")
+                               xAxisTitle: "X Axis",
+                               yAxisTitle: "Y\nAxis")
         data.yAxisMarkSize = 1
         data.alwaysShowZero = true
         return data
     }()
     
+    var stroke: some ShapeStyle {
+        LinearGradient(gradient: Gradient(colors: lineColors),
+                       startPoint: .leading,
+                       endPoint: .trailing)
+    }
+    
     var body: some View {
-//        NavigationView {
             VStack {
                 ScatterChart(data: data,
-                             marker: Circle(),
-                             markerSize: .init(width: 12, height: 12),
-                             lineStyle: .smooth) { day in
+                             marker: marker,
+                             markerSize: markerSize,
+                             lineStyle: .smooth(stroke)) { day in
                     let shouldShow = (day) % 2 == 0
-                    return (shouldShow ? "Day \(day)" : nil, .degrees(-45))
+                    return (shouldShow ? "X-\(day)" : nil, .zero)
                 }
                 .frame(height: 350)
             }
             .navigationBarTitle("pH")
-//        }
     }
 }
 
 struct Example_Previews: PreviewProvider {
     static var previews: some View {
-        Example()
+        Group {
+            Example(lineColors: [Color(.systemPurple), Color(.systemIndigo)],
+                    marker: Circle().strokeBorder(lineWidth: 2).foregroundColor(.blue))
+            
+            Example(lineColors: [Color(.systemPink), Color(.systemPurple)],
+                    marker: Text("❌"),
+                    markerSize: .init(width: 24, height: 24))
+            
+            Example(lineColors: [Color(.systemGray3), Color(.systemGray)],
+                    marker: Text("👽"),
+                    markerSize: .init(width: 24, height: 24))
+            
+            Example(lineColors: [.clear],
+                    marker: Text("🤖"),
+                    markerSize: .init(width: 24, height: 24))
+        }
     }
 }
